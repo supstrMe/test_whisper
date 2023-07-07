e48efe6b0bdda1950a6be537b8eaf18194cdb4e6
Evgeniy Sevostyanihin <sevostyanikhin02@gmail.com>
2023-07-07 12:58:34 +0300

    init

--- app.py
+++ app.py
@@ -2,20 +2,29 @@ from potassium import Potassium, Request, Response
 
 from transformers import pipeline
 import torch
+from io import BytesIO
+import os
+import base64
+
 
 app = Potassium("my_app")
+
 
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
-   
+    model = pipeline(
+        model='openai/whisper-base',
+        chunk_length_s=30,
+        device=device,
+    )
     context = {
         "model": model
     }
 
     return context
+
 
 # @app.handler runs for every call
 @app.handler()
@@ -24,10 +33,23 @@ def handler(context: dict, request: Request) -> Response:
     model = context.get("model")
     outputs = model(prompt)
 
+    mp3BytesString = request.get('mp3BytesString', None)
+    if mp3BytesString == None:
+        return {'message': "No input provided"}
+
+    mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
+    with open('input.mp3', 'wb') as file:
+        file.write(mp3Bytes.getbuffer())
+
+    # Run the model
+    result = model.transcribe("input.mp3")
+    os.remove("input.mp3")
+
     return Response(
-        json = {"outputs": outputs[0]}, 
+        json=result,
         status=200
     )
+
 
 if __name__ == "__main__":
     app.serve()
--- download.py
+++ download.py
@@ -3,9 +3,14 @@
 # In this example: A Huggingface BERT model
 from transformers import pipeline
 
+
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    pipeline(
+        model='openai/whisper-base',
+        chunk_length_s=30
+    )
+
 
 if __name__ == "__main__":
     download_model()
